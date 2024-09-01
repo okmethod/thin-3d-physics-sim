56ceb736c37602f93e1a369c27910cd85781e63a
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-01 18:34:35 +0900

    Updates

---
+++ app/immutable/chunks/paths.wSku5y57.js
@@ -0,0 +1,1 @@
+var s,i;const l=(null==(s=globalThis.__sveltekit_16eui5u)?void 0:s.base)??"/thin-3d-physics-sim",e=(null==(i=globalThis.__sveltekit_16eui5u)?void 0:i.assets)??l;export{e as a,l as b};
--- app/immutable/entry/start.D_Rp8Zec.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/entry.B1QQRAME.js";export{r as start};
